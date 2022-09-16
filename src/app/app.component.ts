2b8f1b60095da018383a982b2122c941354c6549
billytrinh <quanghoa.trinh@gmail.com>
2022-09-16 20:43:09 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,5 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 't2207e-angular';
+  classRoom = "T2207EABC";
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {StudentComponent} from "./student/student.component";
+import {ClassroomComponent} from "./classroom/classroom.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, StudentComponent,ClassroomComponent
   ],
   imports: [
     BrowserModule
